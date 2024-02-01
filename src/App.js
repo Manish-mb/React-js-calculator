b14bb42116363e3869268c5a29f3cfe679c3817e
Manish-mb <manishmb2797@gmail.com>
2024-02-01 18:11:38 +0530

    add new library

--- src/App.js
+++ src/App.js
@@ -1,14 +1,8 @@
 import './mycomponents/App.css';
 import React from "react";
 import Calculator from "./mycomponents/Calculator";
-import ProfileState from "./ProfileState";
 
 
-import {
- BrowserRouter,
- Routes,
- Route
- } from "react-router-dom"; 
 // import Calculator from "./mycomponents/Calculator";
 
 function App() {
